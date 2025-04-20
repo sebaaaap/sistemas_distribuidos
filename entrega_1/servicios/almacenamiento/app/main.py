30a565b66e577213ce2aabd2f762a7b9e8cb9499
sebaa <sebastian.parada1@mail.udp.cl>
2025-04-20 12:49:18 -0400

    agregado el modelo real

--- entrega_1/servicios/almacenamiento/app/main.py
+++ entrega_1/servicios/almacenamiento/app/main.py
@@ -1,27 +1,31 @@
 from fastapi import FastAPI, HTTPException
 from database import events_collection
-from models import Evento
+from models import EventoReal
 from bson import ObjectId
-import os
+from datetime import datetime
 
 app = FastAPI()
+
 @app.post("/eventos/")
-async def crear_evento(evento: Evento):
-    result = events_collection.insert_one(evento.dict())
+async def crear_evento(evento: EventoReal):
+    # Convertimos a dict y añadimos fecha de creación
+    evento_data = evento.dict()
+    evento_data["created_at"] = datetime.utcnow()
+    
+    result = events_collection.insert_one(evento_data)
     return {"id": str(result.inserted_id)}
 
 @app.get("/")
 async def crear_evento():
-    return { "messagge": "hola que tal"}
+    return { "messagge": "hola gai"}
 
 @app.get("/eventos/{evento_id}")
 async def leer_evento(evento_id: str):
     try:
-        # Convierte el string a ObjectId de MongoDB
         obj_id = ObjectId(evento_id)
-        evento = events_collection.find_one({"_id": obj_id})  # Busca con ObjectId
+        evento = events_collection.find_one({"_id": obj_id})
         if evento:
-            evento["_id"] = str(evento["_id"])  # Convierte ObjectId a string para la respuesta
+            evento["_id"] = str(evento["_id"])  # Convertir ObjectId a string
             return evento
     except:
         pass  
--- entrega_1/servicios/almacenamiento/app/models.py
+++ entrega_1/servicios/almacenamiento/app/models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Literal
 from datetime import datetime
 
 class Evento(BaseModel):
@@ -7,3 +7,29 @@ class Evento(BaseModel):
     tipo: str
     ubicacion: dict
     timestamp: datetime = datetime.now()
+    
+class Ubicacion(BaseModel):
+    x: float  # Longitud
+    y: float  # Latitud
+
+class EventoReal(BaseModel):
+    id: str
+    uuid: str
+    country: str
+    city: str
+    street: str
+    location: Ubicacion
+    type: Literal['ROAD_CLOSED']  # Tipo principal del evento
+    subtype: Literal['ROAD_CLOSED_EVENT']  # Subtipo específico
+    speed: int
+    roadType: int
+    inscale: bool
+    confidence: int
+    reliability: int
+    pubMillis: int  # Timestamp en milisegundos
+    timestamp: datetime = datetime.now()  # Fecha de procesamiento en tu sistema
+
+    # Campo calculado para obtener la fecha del evento
+    @property
+    def event_date(self) -> datetime:
+        return datetime.fromtimestamp(self.pubMillis / 1000)
