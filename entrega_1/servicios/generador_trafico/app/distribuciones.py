850034c2056425d7abb7494dce16fc40e5379811
DTC04 <dtapia309@gmail.com>
2025-04-29 17:34:48 -0400

    Realizando pruebas Poisson-LRU

--- entrega_1/servicios/generador_trafico/app/distribuciones.py
+++ entrega_1/servicios/generador_trafico/app/distribuciones.py
@@ -8,13 +8,16 @@ def generar_intervalo():
 
     #Distribución Uniforme 
     # return random.uniform(0.5, 3.0)
-    
-def generar_id_zipf(lista_ids, s=2.0):
+
+  
+"""def generar_id_zipf(lista_ids, s=2.0):
     
     #Devuelve un ID de `lista_ids` siguiendo la distribución de Zipf.    `s` controla el sesgo (mayores valores = más sesgo).
-    
-    n = len(lista_ids)
+     
+      
+   n = len(lista_ids)
     while True:
         idx = np.random.zipf(s) - 1  # zipf genera valores ≥1
         if idx < n:
-            return lista_ids[idx]
+           return lista_ids[idx]
+"""
